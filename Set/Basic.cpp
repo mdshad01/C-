2219ae3c93478645d04d0139da8eb79d18a660c0
Shad <shadalam0024@gmail.com>
2026-03-27 14:06:15 +0530

    unordered_set

--- Set/Basic.cpp
+++ Set/Basic.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 #include<set>
+#include<unordered_set>
 using namespace std;
 
 int main() {
     set<int> s;
+    multiset<int> s1;
+    unordered_set<int> s3;
     s.insert(1);
     s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
     s.insert(3);
     s.insert(4);
     s.insert(5);
---
+++ Set/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+ s.insert(3);
+    s.insert(4);
+    s.insert(5);
